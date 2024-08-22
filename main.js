ea662390ca29498634cff754f045e5d7aa04d088
q1p1 <meshari.algahbban@wakecap.com>
2024-08-22 16:15:16 +0300

    increment

---
+++ main.js
@@ -0,0 +1,6 @@
+// post  | per  increment
+
+/*
+If the variable and before it is ++, it means add and then display the new number
+If ++ is after the variable, it means print the variable and then display the variable
+ */
