49910fbf3cf4ece97c728933db3d4e68924046f4
q1p1 <meshari.algahbban@wakecap.com>
2024-08-22 11:37:35 +0300

    change color

---
+++ main.js
@@ -0,0 +1,15 @@
+/*
+I added this code so that you can add an html tag. 
+And you call it from the code 
+document.querySelector("h1")
+If you want to give it a style, we can call the
+ document.querySelector("h1").style.color = "blue"; 
+"Anything from css" function.
+*/
+document.write("<h2> meshari <h2/>");
+document.querySelector("h2").style.color = "blue";
+/*
+In this code, we will change the color and style by calling the id of the tag from the html,
+ giving it the id, and calling the tag with the name id and changing its properties.
+*/
+document.getElementById("hi").style.color = "red ";
