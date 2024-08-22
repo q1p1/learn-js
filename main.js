45bc480a45c00b01b64ed19eece2eaa56953f944
q1p1 <meshari.algahbban@wakecap.com>
2024-08-22 13:37:19 +0300

    difference between  var let  and const

---
+++ main.js
@@ -0,0 +1,26 @@
+/*
+The difference between 
+var 
+const 
+let 
+ */
+
+/*
+var 
+Redeclare can be done
+
+
+const + let 
+It is not possible to redeclare
+
+
+ */
+
+
+
+ 
+// var redeclare
+
+var  num = 2;
+var  num = 5;
+console.log(num);
