c87c5008dc295c77af61612f99762028479a425d
q1p1 <meshari.algahbban@wakecap.com>
2024-08-22 16:59:24 +0300

    add link

--- main.js
+++ main.js
@@ -4,3 +4,4 @@
 If the variable and before it is ++, it means add and then display the new number
 If ++ is after the variable, it means print the variable and then display the variable
  */
+//https://babeljs.io/repl#?browsers=defaults%2C%20not%20ie%2011%2C%20not%20ie_mob%2011&build=&builtIns=false&corejs=3.21&spec=false&loose=false&code_lz=DYUwLgBAthC8ECICyIDOBDEBLRAoUkAVnIoagvuNOgE4DWArgA4kAGuAPACYgBuAfBE4ALAIyDhOLDAAkAbygBfCBzEB6QSIBMg6RFC0AdlgDmEeYUWqtGzj163WAbly4gA&debug=false&forceAllTransforms=false&modules=false&shippedProposals=false&evaluate=false&fileSize=false&timeTravel=false&sourceType=module&lineWrap=true&presets=env%2Creact%2Cstage-2&prettier=false&targets=&version=7.25.4&externalPlugins=%40babel%2Fplugin-transform-template-literals%407.24.7&assumptions=%7B%7D
