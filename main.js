8a9a70659390faaf06cb5ab43bc1b7acd27e9469
q1p1 <meshari.algahbban@wakecap.com>
2024-08-22 13:06:39 +0300

    typeof

---
+++ main.js
@@ -0,0 +1,25 @@
+/*
+typeof 
+--string 
+-- number 
+-- Array = Object 
+-- Object 
+--Boolean
+-- undefined
+-- null 
+ */
+// String
+console.log(typeof "meshari ");
+// number
+console.log(typeof 3930);
+// Array or Object
+console.log(typeof [2, 2, 34, 4]);
+// object
+console.log(typeof { name: "meshai", age: 30, city: " Tabuk " });
+// Boolean
+console.log(typeof true);
+console.log(typeof false);
+// undefined
+console.log(typeof undefined);
+// null => object
+console.log(typeof null);
