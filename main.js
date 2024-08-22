bb32cce7430bd12d3221b21eebcb8e4dcc6e3144
q1p1 <meshari.algahbban@wakecap.com>
2024-08-22 12:32:22 +0300

    console js

---
+++ main.js
@@ -0,0 +1,12 @@
+//Show message in popup
+window.alert("hi Im leering js");
+//Types of displays on the console
+
+//If you want to type words on the console
+console.log("log");
+//If you want to print an error message in the console
+console.error("error ");
+//If you want to print a table in the console
+console.table(["meshari ", "khaild ", "ahmed "]);
+//If you want to type words and give them a style on the console
+console.log(" %c hi im learning js ", "color: red; font-size:40px; ");
