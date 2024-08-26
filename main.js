65f4649798c21233468e9404a8b2d1edd5bcaa1a
q1p1 <meshari.algahbban@wakecap.com>
2024-08-26 13:01:03 +0300

    numbers

---
+++ main.js
@@ -0,0 +1,18 @@
+//To convert numbers to string
+console.log((100).toString());
+// or
+console.log((100).toString());
+// Fracture control
+console.log((100.92893284664).toFixed(3));
+/*
+Determine the number of fractions
+toFixed(3)
+*/
+
+// is integer  ?
+console.log(Number.isInteger(200));
+// T
+console.log(Number.isInteger(200.2));
+//F
+console.log(Number.isInteger("299"));
+// F
