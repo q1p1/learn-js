02031f7800598c5d50800321d1640a37ce1e2aaa
q1p1 <meshari.algahbban@wakecap.com>
2024-08-26 13:22:07 +0300

    Math

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log(Math.round(20.4));
+console.log(Math.round(20.48));
+
+console.log(Math.ceil(83.2));
+
+console.log(Math.max(200, 444, 523, 144, 1414, 44));
+console.log(Math.min(200, 444, 523, 144, 1414, 44));
