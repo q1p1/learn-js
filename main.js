e800f6c3e4623dd7b2dd17cd0befa75db40ed6d7
q1p1 <meshari.algahbban@wakecap.com>
2024-08-27 16:49:25 +0300

    main

--- main.js
+++
@@ -1,5 +0,0 @@
-//To add an element to the first array
-let myArraye = ["mhamed ", "meshari", "khaild ", "alghabbn "];
-myArraye.unshift("mm");
-myArraye[myArraye.length] = "meshmesh";
-console.log(myArraye);
