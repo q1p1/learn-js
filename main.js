380f2f57c281cc6ac28f58e988cffe7e69df018e
q1p1 <meshari.algahbban@wakecap.com>
2024-08-27 16:16:42 +0300

    array properties

---
+++ main.js
@@ -0,0 +1,5 @@
+//To add an element to the first array
+let myArraye = ["mhamed ", "meshari", "khaild ", "alghabbn "];
+myArraye.unshift("mm");
+myArraye[myArraye.length] = "meshmesh";
+console.log(myArraye);
