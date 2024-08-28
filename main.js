85eea5c7d4aed98fddc547020822ac2441c1a34d
q1p1 <meshari.algahbban@wakecap.com>
2024-08-28 11:31:36 +0300

    splice and  slice array

---
+++ main.js
@@ -0,0 +1,11 @@
+// slice array 
+let myArraye = ["mhamed ", "meshari", "khaild ", "alghabbn ", " meshmesh "];
+
+console.log(myArraye.slice(1));
+console.log(myArraye.slice(1, -1));
+// splice
+myArraye.splice(1,2)
+console.log(myArraye);
+
+
+
