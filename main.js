e3c15345b1259e738cb8ecc18cfa6e7a4cdb9ae2
q1p1 <meshari.algahbban@wakecap.com>
2024-09-02 16:39:46 +0300

    Arrow Function

---
+++ main.js
@@ -0,0 +1,10 @@
+// Arrow Function
+let print = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(print(2, 2));
+// =
+function print1(number1, number2) {
+  return number1 + number2;
+}
+console.log(print(3, 3));
