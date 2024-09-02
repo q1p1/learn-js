220202d77e06b1604c72fbdaf246cb83a6ed2a6a
q1p1 <meshari.algahbban@wakecap.com>
2024-09-02 13:48:40 +0300

    main

--- main.js
+++
@@ -1,8 +0,0 @@
-function info(name, age) {
-  if (age === undefined) {
-    console.error("age!!!");
-    age = "nan";
-  }
-  return `hi ${name} your age is ${age}`;
-}
-console.log(info("meshari"));
