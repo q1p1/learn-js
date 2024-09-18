2afa36530b5e5ead546e7f3af485695d8f36c58d
q1p1 <meshari.algahbban@wakecap.com>
2024-09-18 11:49:24 +0300

    map Methods

---
+++ main.js
@@ -0,0 +1,10 @@
+//map Methods
+
+let mymap = new Map([
+  [10, "namber"],
+  ["meshari", "string"],
+]);
+console.log(mymap.size);
+console.log(mymap.get("meshari"));
+console.log(mymap.delete("meshari"));
+console.log(mymap.has("meshari"));
